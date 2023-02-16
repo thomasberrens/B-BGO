2872511a74764bcbb853a643d26341a023804780
Jason Dikken <jasondikken1@gmail.com>
2023-02-16 12:36:55 +0100

    Camera now moves around a pivot point above the player.

--- assets/Scripts/Controllers/CameraController.cs
+++ assets/Scripts/Controllers/CameraController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Compass _compass;
+    [SerializeField] private GameObject pivotPoint;
+    [SerializeField] private Vector3 positionOffset = new Vector3(0,1,0);
+    
     // Start is called before the first frame update
     void Start()
     {
-        CurrentHeading
+        //float heading = _compass.trueHeading;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(pivotPoint.transform.position, positionOffset, _compass.trueHeading);
     }
 }
