40e261c84450baa5db0ccc31f7f25359bed22c55
Jason Dikken <jasondikken1@gmail.com>
2023-02-16 15:17:05 +0100

    finished camera movement

--- assets/Scripts/Controllers/CameraController.cs
+++ assets/Scripts/Controllers/CameraController.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Compass _compass;
-    [SerializeField] private GameObject pivotPoint;
-    [SerializeField] private Vector3 positionOffset = new Vector3(0,1,0);
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        //float heading = _compass.trueHeading;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(pivotPoint.transform.position, positionOffset, DeviceManager.Instance.Compass.trueHeading);
+        transform.rotation = Quaternion.Euler(new Vector3(0,DeviceManager.Instance.Compass.trueHeading, 0));
     }
 }
