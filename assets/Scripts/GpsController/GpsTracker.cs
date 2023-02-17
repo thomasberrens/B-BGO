7e70bf9d1dbb8f08b847b3af5a0f7c1a02bc53d2
thomas berrens <thomas.berrens@me.com>
2023-02-17 09:16:08 +0100

    made location spoofer and fixed the GetCartesianFromGPS function

--- assets/Scripts/GpsController/GpsTracker.cs
+++ assets/Scripts/GpsController/GpsTracker.cs
@@ -10,42 +10,61 @@ public class GpsTracker : MonoBehaviour
     private Vector3 startGPSLocation;
     private Vector3 currentGPSLocation;
 
+    private LocationService locationService; 
+    
+    [SerializeField] private bool spoofLocation = false;
+    
+    // first latitude, then longitude
+    [SerializeField] private Vector2 spoofLocationCoordinates = new Vector2(0, 0);
+
     private void Start()
     {
         // Get the object's starting position
         startPosition = transform.position;
 
-        // Start the location service
-        Input.location.Start();
+        locationService = DeviceManager.Instance.LocationService;
+        
     }
 
     private bool initialized = false;
 
-    private float extraOffset = 0.01f;
-    
     private void Update()
     {
-        bool isRunning = Input.location.status == LocationServiceStatus.Running;
-
+        bool isRunning = locationService.status == LocationServiceStatus.Running;
+        
         if (!initialized && isRunning)
         {
             Debug.Log("Initialized.");
-            startGPSLocation =  GetCartesianFromGPS(new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude));
+            startGPSLocation =  GetCartesianFromGPS(new Vector2(locationService.lastData.latitude, locationService.lastData.longitude));
             initialized = true;
+            
+            spoofLocationCoordinates.x = locationService.lastData.latitude;
+            spoofLocationCoordinates.y = locationService.lastData.longitude;
+            
             return;
         } else if (!initialized) return;
+        
+        float latitude = locationService.lastData.latitude;
+        float longitude = locationService.lastData.longitude;
+        
+        if (spoofLocation)
+        {
+            latitude = spoofLocationCoordinates.x;
+            longitude = spoofLocationCoordinates.y;
+        }
+
         
         // Update the GPS location
         if (isRunning)
         {
             Debug.Log("Updating location.");
-            currentGPSLocation = GetCartesianFromGPS(new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude));
+            currentGPSLocation = GetCartesianFromGPS(new Vector2(latitude, longitude));
 
             // Calculate the offset between the GPS starting location and the current location
             Vector3 locationOffset = currentGPSLocation - startGPSLocation;
 
 
-            Debug.Log("Offset to add: " + locationOffset.x + ", " + locationOffset.y);
+            Debug.Log("Offset to add: " + locationOffset.x + ", " + locationOffset.y + "," + locationOffset.z);
             
             // Set the object's position to the starting position plus the location offset
             transform.position = startPosition + locationOffset;
@@ -56,13 +75,13 @@ public class GpsTracker : MonoBehaviour
 
     private Vector3 GetCartesianFromGPS(Vector2 gpsLocation)
     {
-        float latitude = (float)gpsLocation.x;
-        float longitude = (float)gpsLocation.y;
+        float latitude = gpsLocation.x;
+        float longitude = gpsLocation.y;
 
         float x = (EarthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Cos(longitude * Mathf.Deg2Rad);
         float y = (EarthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Sin(longitude * Mathf.Deg2Rad);
         float z = (EarthRadius + 0f) * Mathf.Sin(latitude * Mathf.Deg2Rad);
 
-        return new Vector3(x, y, z);
+        return new Vector3(x, 0, y);
     }
 }
