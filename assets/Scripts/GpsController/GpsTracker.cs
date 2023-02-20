94cc5125d677b260db3ec653757aca43ba9a78d3
thomas berrens <thomas.berrens@me.com>
2023-02-20 12:58:25 +0100

    resolved PR feedback

--- assets/Scripts/GpsController/GpsTracker.cs
+++ assets/Scripts/GpsController/GpsTracker.cs
@@ -12,6 +12,10 @@ public class GpsTracker : MonoBehaviour
 
     private LocationService locationService; 
     
+    private const float earthRadius = 6371000f;
+    
+    private bool initialized = false;
+    
     [SerializeField] private bool spoofLocation = false;
     
     // first latitude, then longitude
@@ -25,8 +29,6 @@ public class GpsTracker : MonoBehaviour
         locationService = DeviceManager.Instance.LocationService;
         
     }
-
-    private bool initialized = false;
 
     private void Update()
     {
@@ -53,7 +55,6 @@ public class GpsTracker : MonoBehaviour
             longitude = spoofLocationCoordinates.y;
         }
 
-        
         // Update the GPS location
         if (isRunning)
         {
@@ -71,22 +72,16 @@ public class GpsTracker : MonoBehaviour
         }
     }
     
-    private const float EarthRadius = 6371000f;
+    
 
     private Vector3 GetCartesianFromGPS(Vector2 gpsLocation)
     {
         float latitude = gpsLocation.x;
         float longitude = gpsLocation.y;
 
-        float x = (EarthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Cos(longitude * Mathf.Deg2Rad);
-        float y = (EarthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Sin(longitude * Mathf.Deg2Rad);
-        float z = (EarthRadius + 0f) * Mathf.Sin(latitude * Mathf.Deg2Rad);
+        float x = (earthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Cos(longitude * Mathf.Deg2Rad);
+        float y = (earthRadius + 0f) * Mathf.Cos(latitude * Mathf.Deg2Rad) * Mathf.Sin(longitude * Mathf.Deg2Rad);
 
         return new Vector3(x, 0, y);
-    }
-
-    private void PascalCaseFunction(int camelCase)
-    {
-        
     }
 }
