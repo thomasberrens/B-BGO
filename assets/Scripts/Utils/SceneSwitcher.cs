7437aaf42943827331f0b1dc61509882e85ea7c2
Jason Dikken <jasondikken1@gmail.com>
2023-02-20 12:57:00 +0100

    worked on remove the requested changes from pull request.

--- assets/Scripts/Utils/SceneSwitcher.cs
+++ assets/Scripts/Utils/SceneSwitcher.cs
@@ -7,14 +7,8 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     [SerializeField] private List<GameObject> staticObjects = new List<GameObject>();
-    
     public void SwitchScene(string sceneName) {
-        staticObjects.ForEach(staticObject => DontDestroyOnLoad(staticObject)); ;
+        staticObjects.ForEach(staticObject => DontDestroyOnLoad(staticObject));
         SceneManager.LoadScene(sceneName);
-    }
-
-    public void Awake()
-    {
-        SwitchScene("TestScene");
     }
 }
