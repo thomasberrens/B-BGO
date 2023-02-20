f0727019279efa3de57f93c70fc9a40180edf7dd
thomas berrens <thomas.berrens@me.com>
2023-02-20 13:01:18 +0100

    removed debugs

--- assets/Scripts/ClickableObjects/ClickableObject.cs
+++ assets/Scripts/ClickableObjects/ClickableObject.cs
@@ -10,36 +10,10 @@ public class ClickableObject : MonoBehaviour {
     [SerializeField] private UnityEvent<GameObject> onMouseEnter = new UnityEvent<GameObject>();
     [SerializeField] private UnityEvent<GameObject> onMouseUp = new UnityEvent<GameObject>();
     [SerializeField] private UnityEvent<GameObject> onMouseOver = new UnityEvent<GameObject>();
-
-    private void OnMouseDown()
-    {
-        // This code will be executed when the object is clicked
-        Debug.Log("Object Clicked!");
-        onMouseDown?.Invoke(gameObject);
-    }
-
-    private void OnMouseExit()
-    {
-        Debug.Log("Mouse Exit");
-        
-        onMouseExit?.Invoke(gameObject);
-    }
     
-    private void OnMouseOver()
-    {
-        Debug.Log("Mouse Over");
-        onMouseOver?.Invoke(gameObject);
-    }
-    
-    private void OnMouseEnter()
-    {
-        Debug.Log("Mouse Enter");
-        onMouseEnter?.Invoke(gameObject);
-    }
-
-    private void OnMouseUp()
-    {
-        Debug.Log("Mouse Up");
-        onMouseUp?.Invoke(gameObject);
-    }
+    private void OnMouseDown() => onMouseDown?.Invoke(gameObject);
+    private void OnMouseExit() => onMouseExit?.Invoke(gameObject);
+    private void OnMouseOver() => onMouseOver?.Invoke(gameObject);
+    private void OnMouseEnter() => onMouseEnter?.Invoke(gameObject);
+    private void OnMouseUp() => onMouseUp?.Invoke(gameObject);
 }
