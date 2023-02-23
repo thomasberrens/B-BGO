76c3fce80223265110838609841abeb9d2a88325
Thomas Berrens <thomas.berrens@me.com>
2023-02-23 14:52:01 +0100

    made prototype version of the hide and seek minigame

---
+++ assets/Scripts/Minigame/Minigames/HideAndSeekCharacter.cs
@@ -0,0 +1,22 @@
+﻿
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(ClickableObject))]
+    public class HideAndSeekCharacter : MonoBehaviour
+    {
+        [field: SerializeField] public Image CharacterImage { get; private set; }
+
+        public event Action<GameObject, Image> foundCharacter; 
+
+        private void Awake()
+        {
+        }
+
+        public void FoundObject(GameObject gameObject)
+        {
+            Debug.Log("FOUND");
+            foundCharacter?.Invoke(gameObject, CharacterImage);
+        }
+    }
