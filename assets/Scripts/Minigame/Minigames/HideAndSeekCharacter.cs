1ca37ce6ed1eb4ca597d2caf5218a9c982f56588
Thomas Berrens <thomas.berrens@me.com>
2023-03-16 16:44:24 +0100

    implemented animations, menu/settings/credits screen

--- assets/Scripts/Minigame/Minigames/HideAndSeekCharacter.cs
+++ assets/Scripts/Minigame/Minigames/HideAndSeekCharacter.cs
@@ -7,11 +7,15 @@
     public class HideAndSeekCharacter : MonoBehaviour
     {
         [field: SerializeField] public Image CharacterImage { get; private set; }
+        [field: SerializeField] public Animator Animation { get; set; }
 
         public event Action<GameObject, Image> foundCharacter;
 
         public void FoundObject(GameObject gameObject)
         {
+            Animation.SetBool("isFound", true);
             foundCharacter?.Invoke(gameObject, CharacterImage);
+            
+       
         }
     }
